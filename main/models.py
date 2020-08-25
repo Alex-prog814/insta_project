b8f2eb2000bea59833d04f3d9574c77604d6cf13
alex <aleks.98.x.28@gmail.com>
2020-08-25 15:03:54 +0600

    Add tags

--- main/models.py
+++ main/models.py
@@ -2,11 +2,20 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(primary_key=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='posts')
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='posts')
+    tags = models.ManyToManyField(Tag, related_name='tags', blank=True)
 
     def __str__(self):
         return self.text
@@ -20,5 +29,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-
